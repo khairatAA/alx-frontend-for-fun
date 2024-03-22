a763c853aeaffda62d9afce5f485c297765e5095
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-22 17:40:26 +0100

    fix: Syntax

--- markdown2html.py
+++ markdown2html.py
@@ -2,36 +2,20 @@
 """markdown2html"""
 import sys
 import os
-import markdown
 
 
-def markdown2html(markdown_file, output_file):
+def markdown2html(markdown, output):
     """
     markdown2html: Markdown to HTML
 
-    Args:
-        markdown_file(str): Markdown file
-        output_file(str): output file name
+    args:
+        markdown: Markdown file
+        output: output file name
     """
 
-    if not os.path.isfile(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
+    if not os.path.isfile(markdown):
+        print(f"Missing {markdown}", file=sys.stderr)
         sys.exit(1)
-
-    if len(sys.argv) < 2:
-        print(
-            'Usage: ./markdown2html.py README.md README.html',
-            file=sys.stderr
-            )
-        sys.exit(1)
-
-    with open(markdown_file, 'r') as f:
-        tempMd = f.read()
-
-    tempHTML = markdown.markdown(tempMd)
-
-    with open(output_file, 'w') as f:
-        f.write(tempHTML)
 
     sys.exit(0)
 
@@ -44,5 +28,5 @@ if __name__ == '__main__':
             )
         sys.exit(1)
 
-    # if isinstance(sys.argv[1], str) and isinstance(sys.argv[2], str):
-    markdown2html(sys.argv[1], sys.argv[2])
+    if isinstance(sys.argv[1], str) and isinstance(sys.argv[2], str):
+        markdown2html(sys.argv[1], sys.argv[2])
